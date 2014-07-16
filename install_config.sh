1efd9a915b8e39f4d9d53377b2986bdfe26614bc
Pierre Lucas <pierre.lucas@altie.fr>
2014-07-16 10:13:57 +0200

    mise à jour

--- install_config.sh
+++ install_config.sh
@@ -7,7 +7,8 @@ mkdir ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 wget https://www.languagetool.org/download/LanguageTool-2.5.zip
 mkdir ~/.vim/progExt
-unzip LanguageTool-2.5.zip ~/.vim/progExt/
+unzip LanguageTool-2.5.zip
+mv LanguageTool-2.5/ ~/.vim/progExt/
 rm -rf LanguageTool-2.5.zip
 vim +PluginInstall +qall
 
