1b5c05f2f5ae7aeb5af776a5912048bf48c4cd83
Pierre Lucas <pierre.lucas@altie.fr>
2014-09-12 22:49:23 +0200

    Cleaning git

--- install_config.sh
+++ install_config.sh
@@ -1,15 +1,53 @@
 #! /bin/sh
 
-ln -s vim/vimrc ~/.vimrc
-mkdir ~/.vim/
-ln -s vim/config/ ~/.vim/
-mkdir ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-wget https://www.languagetool.org/download/LanguageTool-2.5.zip
-mkdir ~/.vim/progExt
-unzip LanguageTool-2.5.zip
-mv LanguageTool-2.5/ ~/.vim/progExt/
-rm -rf LanguageTool-2.5.zip
+startingDir=${PWD}
+
+## Config vim
+#delete old configuration files
+rm ${HOME}/.vimrc
+rm -rf ${HOME}/.vim
+
+#create symlinks to the configuration files
+ln -s ${PWD}/vim/.vimrc ${HOME}/.vimrc
+ln -s ${PWD}/vim/.vim ${HOME}/.vim
+
+#install the software that manages the plugins for vim
+mkdir ${HOME}/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+
+#install language spelling
+mkdir ${HOME}/spell
+for lang in fr en
+do
+  for ext in spl sug
+  do
+    wget -P ${HOME}/.vim/spell  http://ftp.vim.org/vim/runtime/spell/${lang}.utf-8.${ext}
+    wget -P ${HOME}/.vim/spell  http://ftp.vim.org/vim/runtime/spell/${lang}.latin1.${ext}
+  done
+done
+
+#install language assistance
+mkdir ${HOME}/.vim/progExt
+wget https://www.languagetool.org/download/LanguageTool-2.6.zip
+
+# unzip -o : overwrites the unzipped files if it already exists
+unzip -o LanguageTool-2.6.zip
+mv LanguageTool-2.6 ${HOME}/.vim/progExt/
+rm -rf LanguageTool-2.6*
 vim +PluginInstall +qall
 
-ln -s zsh/zshrc ~/.zshrc
+cd ${startingDir}
+
+## Config zsh
+#delete old configuration files
+rm ${HOME}/.zshrc
+
+#create symlinks to the configuration files
+ln -s ${PWD}/zsh/.zshrc ${HOME}/.vimrc
+sudo cp ${PWD}/zsh/zshrc /etc/zsh/
+sudo cp ${PWD}/zsh/zshenv /etc/zsh/
+sudo cp ${PWD}/zsh/zlogin /etc/zsh/
+sudo cp ${PWD}/zsh/zlogout /etc/zsh/
+sudo cp ${PWD}/zsh/dir_colors /etc/
+
+cd ${startingDir}
