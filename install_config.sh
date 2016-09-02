fccc5ac0992be7ebce046a2d1971c2b398614e84
Pierre Lucas <pierre.lucas@altie.fr>
2016-09-02 14:05:52 +0200

    comment language option for vim

--- install_config.sh
+++ install_config.sh
@@ -18,25 +18,25 @@ mkdir ${HOME}/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 
 #install language spelling
-mkdir ${HOME}/spell
-for lang in fr en
-do
-  for ext in spl sug
-  do
-    wget -P ${HOME}/.vim/spell  http://ftp.vim.org/vim/runtime/spell/${lang}.utf-8.${ext}
-    wget -P ${HOME}/.vim/spell  http://ftp.vim.org/vim/runtime/spell/${lang}.latin1.${ext}
-  done
-done
+# mkdir ${HOME}/spell
+# for lang in fr en
+# do
+#   for ext in spl sug
+#   do
+#     wget -P ${HOME}/.vim/spell  http://ftp.vim.org/vim/runtime/spell/${lang}.utf-8.${ext}
+#     wget -P ${HOME}/.vim/spell  http://ftp.vim.org/vim/runtime/spell/${lang}.latin1.${ext}
+#   done
+# done
 
 #install language assistance
-mkdir ${HOME}/.vim/progExt
-wget https://www.languagetool.org/download/LanguageTool-2.6.zip
+# mkdir ${HOME}/.vim/progExt
+# wget https://www.languagetool.org/download/LanguageTool-2.6.zip
 
 # unzip -o : overwrites the unzipped files if it already exists
-unzip -o LanguageTool-2.6.zip
-mv LanguageTool-2.6 ${HOME}/.vim/progExt/
-rm -rf LanguageTool-2.6*
-vim +PluginInstall +qall
+# unzip -o LanguageTool-2.6.zip
+# mv LanguageTool-2.6 ${HOME}/.vim/progExt/
+# rm -rf LanguageTool-2.6*
+# vim +PluginInstall +qall
 
 sudo ln -s ${HOME}/.vim /root/.vim
 sudo ln -s ${HOME}/.vimrc /root/.vimrc
